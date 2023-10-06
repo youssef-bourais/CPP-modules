d97c7e959ec56aba56e6874571419a6466878bda
youssefbourais0310 <youssef.bourais0310@gmail.com>
2023-10-06 17:56:23 +0100

    fd

--- CPP02/ex00/Fixed.cpp
+++ CPP02/ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: ybourais <ybourais@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 16:27:07 by ybourais          #+#    #+#             */
-/*   Updated: 2023/10/06 17:43:01 by ybourais         ###   ########.fr       */
+/*   Updated: 2023/10/06 17:54:36 by ybourais         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,23 @@
 
 Fixed::Fixed():integer(0)
 {
+    std::cout<<"Default constructor called"<<std::endl;
 }
 
 Fixed::~Fixed()
-{
+{ 
+    std::cout<<"Destructor called"<<std::endl;
 }
 
 Fixed::Fixed(Fixed const &src)
 {
+    std::cout<<"Copy constructor called"<<std::endl;
     *this = src;
 }
 
 Fixed &Fixed::operator=(Fixed const &s)
-{
+{ 
+    std::cout<<"Copy assignment operator calle"<<std::endl;
     if(this != &s)
         this->integer = s.getRawBits();
     return *this;
@@ -34,11 +38,13 @@ Fixed &Fixed::operator=(Fixed const &s)
 
 int Fixed::getRawBits() const
 {
+    std::cout<<"getRawBits member function called"<<std::endl;
     return this->integer;
 }
 
 void Fixed::setRawBits(int const raw)
 {
+    std::cout<<"setRawBits member function called"<<std::endl;
     this->integer = raw;
 }
 
