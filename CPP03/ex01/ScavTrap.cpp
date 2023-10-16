e9cbd96d3663bbc18e9319e903e8fde25b9120c5
youssefbourais0310 <youssef.bourais0310@gmail.com>
2023-10-16 16:15:02 +0100

    d

--- CPP03/ex01/ScavTrap.cpp
+++ CPP03/ex01/ScavTrap.cpp
@@ -6,8 +6,28 @@
 /*   By: ybourais <ybourais@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 13:14:04 by ybourais          #+#    #+#             */
-/*   Updated: 2023/10/16 13:14:08 by ybourais         ###   ########.fr       */
+/*   Updated: 2023/10/16 16:11:41 by ybourais         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "ScavTrap.hpp"
+
+
+ScavTrap::ScavTrap(std::string &name): ClapTrap(name)
+{
+    std::cout<< "constructor called for derived class"<<std::endl;
+}
+
+ScavTrap::~ScavTrap()
+{
+    std::cout<< "deconstructor called for derived class"<<std::endl;
+}
+
+void ScavTrap::attack(const std::string &target)
+{
+    std::cout<<"derived class attack"<<std::endl;
+    ClapTrap::attack(target);
+}
+
 
 
--- CPP03/ex01/ScavTrap.hpp
+++ CPP03/ex01/ScavTrap.hpp
@@ -6,8 +6,21 @@
 /*   By: ybourais <ybourais@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 13:14:19 by ybourais          #+#    #+#             */
-/*   Updated: 2023/10/16 13:14:21 by ybourais         ###   ########.fr       */
+/*   Updated: 2023/10/16 16:11:43 by ybourais         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SCAVTRAP_HPP
+#define SCAVTRAP_HPP
 
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap
+{
+    public:
+        ScavTrap(std::string &name);
+        ~ScavTrap();
+        void attack(const std::string& target);
+};
+
+#endif 
