368eed04c2f9dee40ac3ecfee260861dce5228e8
youssefbourais0310 <youssef.bourais0310@gmail.com>
2023-12-09 19:16:07 +0100

    done

--- CPP05/ex00/main.cpp
+++ CPP05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ybourais <ybourais@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 10:38:27 by ybourais          #+#    #+#             */
-/*   Updated: 2023/12/09 18:58:07 by ybourais         ###   ########.fr       */
+/*   Updated: 2023/12/09 19:14:40 by ybourais         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,31 @@
 int main() 
 { 
     Bureaucrat a("yassine"); 
-
+    Bureaucrat b("youssef"); 
+    
     try 
     {
-        a.set_grade(144);
         a.set_grade(300);
+        a.increment_grade();
         std::cout<<a;
         /* a.increment_grade(); */
         /* a.decrement_grade(); */
     } 
-    catch (const Bureaucrat::GradeTooHighException &e) 
+    catch (std::exception &e) 
     {
         std::cout << "Error: " << e.what() << std::endl;
     }
-    catch (const Bureaucrat::GradeTooLowException& e) 
+    try 
+    {
+        b.set_grade(4);
+        b.increment_grade();
+        std::cout<<b;
+        /* a.increment_grade(); */
+        /* a.decrement_grade(); */
+    } 
+    catch (std::exception &e) 
     {
         std::cout << "Error: " << e.what() << std::endl;
-    } 
+    }
 }
 
