0a98a63ef4ea9bcced9afd2f86518eab349e1d5d
youssefbourais0310 <youssef.bourais0310@gmail.com>
2023-12-29 02:57:52 +0100

    finishing ex02

--- CPP07/ex02/Array.hpp
+++ CPP07/ex02/Array.hpp
@@ -6,19 +6,81 @@
 /*   By: ybourais <ybourais@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 05:44:06 by ybourais          #+#    #+#             */
-/*   Updated: 2023/12/28 06:31:42 by ybourais         ###   ########.fr       */
+/*   Updated: 2023/12/29 02:55:43 by ybourais         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
+#include <iostream>
 
+template <typename T>
+class Array
+{
+    private:
+        T *data;
+    public:
+        Array();
+        Array(unsigned int n);
+        Array(Array<T> &copy);
+        Array &operator=(Array<T> &src);
+        T& operator[](unsigned int index); 
+        
+        unsigned int size() const;
+};
 
+template <typename T>
+Array<T>::Array()
+{
+    this->data = NULL;
+}
 
+template <typename T>
+Array<T>::Array(unsigned int n)
+{
+    this->data = new T[n];
+}
 
+template <typename T>
+Array<T>::Array(Array <T> &copy)
+{
+    if(data != NULL)
+        delete[] data;
+    data = new T[copy.size()]; 
+    for(int i = 0;i < copy.size();i++)
+        data[i] = copy.data[i];
+}
 
+template <typename T>
+Array<T> &Array<T>::operator=(Array<T> &src)
+{
+    if(this != &src)
+    {
+        if(data != NULL)
+            delete[] data;
+        data = new T[src.size()];
+        for(int i = 0;i < src.size();i++)
+            data[i] = src.data[i];
+    }
+    return *this;
+}
 
+template <typename T>
+unsigned int Array<T>::size() const
+{
+    return (sizeof(data )/sizeof(T));
+}
+
+template <typename T>
+T &Array<T>::operator[](unsigned int index) 
+{
+        if (index >= size()) 
+        {
+            throw std::out_of_range("Index out of bounds");
+        }
+        return data[index];
+}
 
 
 
--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -6,8 +6,61 @@
 /*   By: ybourais <ybourais@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 05:43:46 by ybourais          #+#    #+#             */
-/*   Updated: 2023/12/28 05:43:48 by ybourais         ###   ########.fr       */
+/*   Updated: 2023/12/29 02:57:07 by ybourais         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Array.hpp"
+#include <stdexcept>
+
+int main() 
+{
+    try 
+    {
+        // Test default construction
+        Array<int> emptyArray;
+        std::cout << "Default-constructed array size: " << emptyArray.size() << std::endl;
+
+        // Test construction with a specified size
+        Array<int> intArray(5);
+        std::cout << "Array size after construction: " << intArray.size() << std::endl;
+
+        // Test subscript operator and size function
+        for (unsigned int i = 0; i < intArray.size(); ++i) 
+        {
+            intArray[i] = i * 2;
+            std::cout << "Element at index " << i << ": " << intArray[i] << std::endl;
+        }
+
+        // Test copy construction
+        Array<int> copiedArray(intArray);
+        std::cout << "Copied array size: " << copiedArray.size() << std::endl;
+
+        // Test assignment operator
+        Array<int> assignedArray = intArray;
+        std::cout << "Assigned array size: " << assignedArray.size() << std::endl;
+
+        // Test modification after copy and assignment
+        intArray[0] = 999;  // Modify original array
+        std::cout << "Original array element at index 0: " << intArray[0] << std::endl;
+        std::cout << "Copied array element at index 0: " << copiedArray[0] << std::endl;
+
+        // Test out-of-bounds access
+        try 
+        {
+            intArray[intArray.size()] = 42;  // Should throw an exception
+        } 
+        catch (const std::exception& e) 
+        {
+            std::cerr << "Exception caught: " << e.what() << std::endl;
+        }
+
+    } 
+    catch (const std::exception& e) 
+    {
+        std::cerr << "Exception caught: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
 
