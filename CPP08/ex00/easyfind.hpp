e5e5814ec1ab0b204e7054577f15bf4ada3c442f
youssefbourais0310 <youssef.bourais0310@gmail.com>
2024-01-02 07:38:06 +0100

    finishing ex00

--- CPP08/ex00/easyfind.hpp
+++ CPP08/ex00/easyfind.hpp
@@ -6,41 +6,31 @@
 /*   By: ybourais <ybourais@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/01 23:59:07 by ybourais          #+#    #+#             */
-/*   Updated: 2024/01/02 04:28:06 by ybourais         ###   ########.fr       */
+/*   Updated: 2024/01/02 07:08:13 by ybourais         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
-
 #include <iostream>
 
-#include <list>
-#include <vector>
+class ElementNotFound : public std::exception 
+{
+public:
+    virtual const char* what() const throw() 
+    {
+        return "Element not found in container.";
+    }
+};
 
 template <typename T>
-int easyfind(const T& container, int n)
+typename T::const_iterator easyfind(const T &container, int n)
 {
-    int size = sizeof(container);
-    std::vector<int>::iterator it = container.begin();
-    std::cout<< *it<<" hello"<<n << size<<std::endl;
-
-    /* int *a = std::find(std::begin(container), std::end(container), n); */
-    /* std::cout<< &(*a)<<std::endl;  */
-    return 0;
+    typename T::const_iterator it = std::find(std::begin(container), std::end(container), n);
+    if(it == container.end())
+        throw ElementNotFound();
+    return it;
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- CPP08/ex00/main.cpp
+++ CPP08/ex00/main.cpp
@@ -6,10 +6,11 @@
 /*   By: ybourais <ybourais@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/30 00:29:59 by ybourais          #+#    #+#             */
-/*   Updated: 2024/01/02 04:27:54 by ybourais         ###   ########.fr       */
+/*   Updated: 2024/01/02 07:36:56 by ybourais         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <vector>
 #include <list>
 #include "easyfind.hpp"
 
@@ -17,16 +18,16 @@ int main()
 {
     int arr[] = {2,4,4,5,6,8};
     std::vector<int> vector(arr, arr + sizeof(arr)/sizeof(arr[0]));
-    /* vector.push_back(10); */
-    /* vector.push_back(10); */
-    /* vector.push_back(10); */
-    /* vector.push_back(10); */
-    /* vector.push_back(10); */
-    /* std::cout<< *vector.begin()<<std::endl; */
-    
-    std::cout<< &(*(arr + 3))<<std::endl;
-    easyfind(vector, 5);
-
-    
+    /* std::vector<int> vector = {2,4,4,5,6,8}; */
+   
+    try
+   {
+       std::vector<int>::const_iterator result = easyfind(vector, -5);
+       std::cout<<*result<<std::endl;
+   }
+   catch(std::exception &e)
+   {
+       std::cout<< e.what()<<std::endl;
+   }
 }
 
