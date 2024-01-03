ef7dee8f98c89958341264cb7340d5362257307c
youssefbourais0310 <youssef.bourais0310@gmail.com>
2024-01-04 00:59:45 +0100

    ready to push

--- CPP08/ex02/MutantStack.hpp
+++ CPP08/ex02/MutantStack.hpp
@@ -6,8 +6,70 @@
 /*   By: ybourais <ybourais@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 08:15:42 by ybourais          #+#    #+#             */
-/*   Updated: 2024/01/03 08:15:42 by ybourais         ###   ########.fr       */
+/*   Updated: 2024/01/04 00:59:11 by ybourais         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T> 
+{
+    public:
+        MutantStack();
+        ~MutantStack();
+        MutantStack(const MutantStack &copy);
+        MutantStack &operator=(const MutantStack &src);
+        
+        typename std::stack<T>::container_type::iterator end();
+        typename std::stack<T>::container_type::iterator begin();
+
+};
+
+template <typename T>
+MutantStack<T>::MutantStack() 
+{
+}
+
+
+template <typename T>
+MutantStack<T>::~MutantStack() 
+{
+}
+
+template <typename T>
+MutantStack<T>::MutantStack(const MutantStack &copy)
+{
+    *this = copy;
+}
+
+template <typename T>
+MutantStack<T> &MutantStack<T>::operator=(const MutantStack &src)
+{
+    if(this != &src)
+    {
+        while (!this->empty()) 
+            this->pop();
+       this->c = src.c; 
+    }
+    return *this;
+}
+
+template <typename T>
+typename std::stack<T>::container_type::iterator MutantStack<T>::begin()
+{
+    return (this->c.begin());
+}
+
+template <typename T>
+typename std::stack<T>::container_type::iterator MutantStack<T>::end()
+{
+    return (this->c.end());
+}
+
+
+#endif
 
--- CPP08/ex02/main.cpp
+++ CPP08/ex02/main.cpp
@@ -6,8 +6,32 @@
 /*   By: ybourais <ybourais@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 08:13:54 by ybourais          #+#    #+#             */
-/*   Updated: 2024/01/03 08:13:55 by ybourais         ###   ########.fr       */
+/*   Updated: 2024/01/04 00:59:09 by ybourais         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include "MutantStack.hpp"
 
+
+
+
+int main()
+{
+    MutantStack<int> A;
+    std::stack<int>::container_type::iterator itrBegin;
+    std::stack<int>::container_type::iterator itrEnd;
+    
+    int i = 1;
+    while(i <= 30)
+        A.push(i++);
+    
+    itrBegin = A.begin();
+    itrEnd = A.end();
+    while(itrBegin != itrEnd)
+    {
+        std::cout<< *itrBegin<<" ";
+        itrBegin++;
+    }
+    std::cout<<'\n';
+}
